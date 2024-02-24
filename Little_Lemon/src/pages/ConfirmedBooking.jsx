98889ff6f283b858847e487e5b90af0a99a2ed52
shanukafer98 <141110476+shanukafer98@users.noreply.github.com>
2024-02-24 11:24:25 +0530

    Update ConfirmedBooking.jsx

--- Little_Lemon/src/pages/ConfirmedBooking.jsx
+++ Little_Lemon/src/pages/ConfirmedBooking.jsx
@@ -3,7 +3,7 @@ export default function ConfirmedBooking() {
       <main className="confirmed-main">
        <p className="title confirmed-title">Booking Confirmed!!!</p>
        <p className="subtitle confirmed-subtitle">Please provide your name and contact number at the reception upon arrival</p>
-       <p className="subtitle confirmed-subtitle">We are excited to serve you</p>
+       <p className="subtitle confirmed-subtitle">We are excited to serve you!!!</p>
       </main>
     )
   }
