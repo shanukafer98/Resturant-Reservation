04e4299cb50c128318666923a741c223d942a752
shanukafer98 <141110476+shanukafer98@users.noreply.github.com>
2024-02-24 10:52:12 +0530

    Add and delete image files, update Vite configuration and main.jsx

---
+++ Little_Lemon/src/components/Testimonials.jsx
@@ -0,0 +1,48 @@
+export default function Testimonials() {
+    return (
+      <div className="testimonials-parent">
+        <p className="testimonials-title">Testimonials</p>
+        <div className="testimonials">
+            <div className="testimonial">
+                <img className="reviewer-img" src="/images/reviewer1.png" alt="greek salad"/>
+                <div className="name-rating">
+                    <p className="reviewer-name">Nikhil patak</p>
+                    <p className="reviewer-rating">4.3/5</p>
+                </div>
+                <div className="review-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
+                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
+                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                </div>
+            </div>
+            <div className="testimonial">
+                <img className="reviewer-img" src="/images/reviewer2.png" alt="greek salad"/>
+                <div className="name-rating">
+                    <p className="reviewer-name">Maria Swe</p>
+                    <p className="reviewer-rating">4.7/5</p>
+                </div>
+                <div className="review-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
+                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
+                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                </div>
+            </div>
+            <div className="testimonial">
+                <img className="reviewer-img" src="/images/reviewer3.png" alt="greek salad"/>
+                <div className="name-rating">
+                    <p className="reviewer-name">Nikila Sez</p>
+                    <p className="reviewer-rating">4.5/5</p>
+                </div>
+                <div className="review-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa, 
+                        semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper, 
+                        diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+  
+
+  
